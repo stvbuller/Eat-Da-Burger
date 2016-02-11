9e29b73c275fd3fb68cf2f2541a52b51a6d63b5d
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-11 16:08:59 -0500

    app setup step is completed, the server works

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 8090;
 
-var connect        = require('connect')
-var methodOverride = require('method-override')
+
  
 // override with the X-HTTP-Method-Override header in the request 
 app.use(methodOverride('X-HTTP-Method-Override'))
+//set up for handlebars
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: false}));
 
-
-
+//var connect        = require('connect')
 
  
 //connection.end();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+        //res.render('index', lineage);
+        res.send('Hello World');
+});
  
 app.listen(PORT, function() {
   console.log("Listening at %s", PORT);
