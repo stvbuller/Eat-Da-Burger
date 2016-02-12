d8a698fb61c6283a76dfc4ee53898e30454525d3
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-12 18:50:41 -0500

    added routes to burgers_controller.js

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -1,2 +1,55 @@
 var express = require('express');
-var router = require('../models/burger.js');
+var app = express()
+//var burger = require('../models/burger.js');
+var shwMenu = require("../config/orm.js");
+
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//app.set('views', __dirname + '../views');
+// app.engine('handlebars', expressHandlebars({
+//             defaultLayout:'main', 
+//             layoutsDir: '/Users/steven/RCB _Class/GitRepository/Homework/Eat-Da-Burger/burger/views/layouts'
+//         }));
+
+// app.engine('.hbs', exphbs({
+//         defaultLayout: 'main', 
+//         extname: '.hbs',
+//         layoutsDir:'server/views/layouts',
+//         partialsDir:'server/views/partials'
+// }));
+
+// app.engine('hbs', HBS({
+//             extname:'hbs', 
+//             defaultLayout:'main.hbs', 
+//             layoutsDir: PATH.resolve(__dirname, 'templates/layouts')
+//         }));
+
+var PORT = process.env.PORT || 8090;
+
+
+app.get('/',function(req,res){
+  //show home page
+    var results = shwMenu.showMenu();
+    var data = {
+      burgerData: results
+
+    }
+    //res.send(results);
+    res.render('index', data);
+});
+
+app.post('/create', function(req, res) {
+    //add a burger
+    res.redirect('/');
+});
+
+app.post('/update/:id', function(req, res) {
+    //devour a burger, change the devoured field to 1 (true)
+    res.redirect('/');
+});
+
+app.listen(PORT, function(){
+  console.log('Listening on %s', PORT)
+})
