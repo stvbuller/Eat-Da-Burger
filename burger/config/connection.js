808a423ad472c1c22ec47862a58a7f837129220c
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-12 17:10:05 -0500

    added queries to orm.js

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,24 +1,26 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host:'localhost',
-  port: 3306,
-  user:'root',
-  password: '',
-  database:'burgers_db'
-});
+exports.connectDb = function() {
+
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    user:'root',
+    password: '',
+    database:'burgers_db'
+  });
+  return connection;
+ }
  
- 
+
+//used to test the database connection
 // connection.connect(function(err) {
 //   if(err) {
 //     console.log("Error", err.stack);
 //   }
-  
 //   console.log("Connected as id: %s", connection.threadId)
 // });
 
-
-//used to test the database connection
 // connection.query('SELECT * FROM burgers', function(err, rows, fields) {                                                   
 //     if (err) throw err;
-//     console.log("The burger test :" + rows[0].burger_name);                 
+//     console.log("The burger connection test :" + rows[0].burger_name);                 
 // });
--- burger/config/orm.js
+++ burger/config/orm.js
@@ -1,8 +1,43 @@
-var conn1 = require("./connection.js");
+var connectMysql = require("./connection.js");
+var connection = connectMysql.connectDb();
+
+connection.connect(function(err) {
+  if(err) {
+    console.log("Error", err.stack);
+  }
+  console.log("Connected as id: %s", connection.threadId)
+});
 
 
+exports.addBurger = function(burger){
+  var burgerName = burger;
+  var mySQLQuery = "INSERT INTO burgers (burger_name) VALUES ('" + burgerName + "')";
+  connection.query(mySQLQuery, function(err) {
+    if (err) {
+      throw err
+    }
+  });
+}
 
+exports.devourBurger = function(id) {
+  var id = id;
+  connection.query("UPDATE burgers SET devoured=1 WHERE id=?", [id], function(err, rows, fields) {                                                   
+    if (err) throw err;                
+  }); 
+}
 
+//used to test the database queryies
+//devourBurger(8);
+//addBurger("chedder");
+//used to test the database connection
+// connection.connect(function(err) {
+//   if(err) {
+//     console.log("Error", err.stack);
+//   }
+//   console.log("Connected as id: %s", connection.threadId)
+// });
 
-
-
+// connection.query('SELECT * FROM burgers', function(err, rows, fields) {                                                   
+//     if (err) throw err;
+//     console.log("The burger connection test :" + rows[0].burger_name);                 
+// });
