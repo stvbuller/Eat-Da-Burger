4547c07089c6b7730a4ebd3b12f32fa9035f8ab9
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-12 17:18:16 -0500

    added comments

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -26,16 +26,10 @@ exports.devourBurger = function(id) {
   }); 
 }
 
-//used to test the database queryies
+//used to test the database queryies and functions
 //devourBurger(8);
 //addBurger("chedder");
-//used to test the database connection
-// connection.connect(function(err) {
-//   if(err) {
-//     console.log("Error", err.stack);
-//   }
-//   console.log("Connected as id: %s", connection.threadId)
-// });
+
 
 // connection.query('SELECT * FROM burgers', function(err, rows, fields) {                                                   
 //     if (err) throw err;
