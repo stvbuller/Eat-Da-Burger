83bc55dd11357d1a750fc7bdfc401135b58ddb4d
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-14 12:38:34 -0500

    changed variables in server.js

--- burger/server.js
+++ burger/server.js
@@ -1,20 +1,24 @@
 var express = require('express');
-var app = express();
-var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var PORT = process.env.PORT || 8090;
+var app = express();
+//app.use(express.static(__dirname + '/public'));
 
+var PORT = process.env.PORT || 8090; 
 
+app.use(bodyParser.urlencoded({extended: false}));
  
 // override with the X-HTTP-Method-Override header in the request 
 app.use(methodOverride('X-HTTP-Method-Override'))
 //setup for handlebars
+var expressHandlebars = require('express-handlebars');
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended: false}));
 
+
+// var routes = require('./controllers/burgers_controller.js');
+// app.use('/', routes);
 
 app.get('/', function (req, res) {
         res.send('Hello World');
