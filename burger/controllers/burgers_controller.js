70080a6d910f90964a557222f13cf9ddde767fcd
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-14 12:58:26 -0500

    added router to burgers_controller.js

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -1,25 +1,36 @@
 var express = require('express');
-var app = express()
+//var app = express()
 var router = express.Router();
+var thing = require('../models/burger.js';)
 
 
-app.get('/',function(req,res){
-  //show home page
-    var results = shwMenu.showMenu();
-    var data = {
-      burgerData: results
-
-    }
-    //res.send(results);
-    res.render('index', data);
+router.get('/', function(req, res) {
+    thing.show(function(data){
+        res.render('index', {data : data});
+    });
 });
 
-app.post('/create', function(req, res) {
-    //add a burger
-    res.redirect('/');
-});
 
-app.post('/update/:id', function(req, res) {
-    //devour a burger, change the devoured field to 1 (true)
-    res.redirect('/');
-});
+
+
+
+// app.get('/',function(req,res){
+//   //show home page
+//     var results = shwMenu.showMenu();
+//     var data = {
+//       burgerData: results
+
+//     }
+//     //res.send(results);
+//     res.render('index', data);
+// });
+
+// app.post('/create', function(req, res) {
+//     //add a burger
+//     res.redirect('/');
+// });
+
+// app.post('/update/:id', function(req, res) {
+//     //devour a burger, change the devoured field to 1 (true)
+//     res.redirect('/');
+// });
